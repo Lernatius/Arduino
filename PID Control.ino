52987fba13bb12aed80880cff6c4123c6dbbfc00
Lernatius <besparmakm@itu.edu.tr>
2019-10-18 14:02:31 +0300

    Update PID Control.ino

--- PID Control.ino
+++ PID Control.ino
@@ -11,7 +11,7 @@ void loop() {
   
         double Input, SetPoint, Output, virtualOut; // Input : motor hızı, setpoint: hedeflene hız değeri, Output : çıkış değeri
         double Kp, Ki, Kd; // PID coefficients
-        double SampleTime;  // sampling time 1/frequency
+        double SampleTime;  // sampling time : 1/frequency
         double error, integral, derivative;
         double outMin = 0, outMax = 255, Imax = 255, Imin = 0;   
         double lastOut, lastIn;
@@ -20,13 +20,13 @@ void loop() {
         {
             error = SetPoint - Input;  //Calculate error
 
-            integral += (error * SampleTime);
+            integral += (error * SampleTime);  // Calculate integrator participant of the controller output
             if (integral > Imax) { integral = Imax; }
-            else if ( integral < Imin) { integral = Imin; } // Anti windup proces
+            else if ( integral < Imin) { integral = Imin; } // Anti windup control
 
             derivative = (Input - lastIn) / SampleTime;   // prevents derivative kick 
 
-            virtualOut = Kp * error + Ki * integral - Kd * derivative;
+            virtualOut = Kp * error + Ki * integral - Kd * derivative;  // Raw Control Output 
 
             if (virtualOut >= outMax) { virtualOut = outMax; }
             else if (virtualOut <= outMin) { virtualOut = outMin; }
